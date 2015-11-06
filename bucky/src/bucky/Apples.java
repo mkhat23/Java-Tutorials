c8e1eaf7e91f996d332cbcdc1c7cd3134b32188c
mkh10 <mandeep.khataria@bskyb.com>
2015-11-06 09:59:04 +0000

    Corrected typo

--- bucky/src/bucky/Apples.java
+++ bucky/src/bucky/Apples.java
@@ -40,7 +40,7 @@ public class Apples {
 		/*
 		 * int age = 45;
 		 * 
-		 * if (age >= 60) { System.out.println("You are a senior ciizen"); }
+		 * if (age >= 60) { System.out.println("You are a senior citizen"); }
 		 * else if (age >= 50) { System.out.println("You are in your 50's"); }
 		 * else if (age >= 40) { System.out.println("You are in your 40's"); }
 		 * 
